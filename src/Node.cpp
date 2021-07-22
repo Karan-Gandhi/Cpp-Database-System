781a7e86113a19a0d6e162bfa08952e9e0b5cf2c
Karan Gandhi <karangandhi.programming@gmail.com>
2021-07-22 10:45:46 +0530

    try with FILE*

--- src/Node.cpp
+++ src/Node.cpp
@@ -52,30 +52,70 @@ class Node {
         }
 
         void read_from_file(std::string filename) {
-            std::ifstream ifstream;
-            ifstream.open(filename);
-            if (!ifstream) return;
+            FILE* file = fopen(filename.c_str(), "w");
 
-            std::getline(ifstream, id);
-            ifstream >> data_size;
-            char* buff = new char[data_size];
+            read_file_recursively(file);
+
+            fclose(file);
+        }
+
+    private:
+        void read_file_recursively(FILE* file) {
+            if (feof(file)) return;
+            
+            id = get_string(file);
+            
+            data_size = atoi(get_string(file).c_str());
 
             std::cout << id << std::endl;
-            if (!data_size) goto read_children;
             std::cout << data_size << std::endl;
             
-            ifstream.read(buff, data_size);
-            value = buff;
+            fread(value, data_size, 1, file);
+            // ifstream.read(buff, data_size);
+            if (data_size) std::cout << *(std::string*) value << std::endl;
             
-            read_children:
-            int number_of_children;
-            ifstream >> number_of_children;
-            for (int i = 0; i < number_of_children; i++) {
-                Node* child = new Node("", nullptr, 0);
-                child->read_from_file(filename);
-                children.push_back(child);
-            }
-            
-            ifstream.close();
+        // read_children:
+            // int number_of_children;
+            // ifstream >> number_of_children;
+            // std::cout << number_of_children << std::endl;
+            // for (int i = 0; i < number_of_children; i++) {
+            //     Node* child = new Node("", nullptr, 0);
+            //     child->read_file_recursively(ifstream);
+            //     children.push_back(child);
+            // }
         }
+
+        std::string get_string(FILE* file) {
+            std::string res;
+            char* buff;
+            fgets(buff, 10000, file);
+            res = buff;
+            return res;
+        }
+
+        // void read_file_recursively(std::ifstream& ifstream) {
+        //     if (ifstream.eof()) return;
+
+        //     std::getline(ifstream, id);
+
+        //     ifstream >> data_size;
+        //     char* buff = new char[data_size];
+
+        //     // std::cout << id << std::endl;
+        //     // std::cout << data_size << std::endl;
+            
+        //     ifstream.read(buff, data_size);
+        //     value = buff;
+        //     if (data_size) std::cout << *(std::string*) value << std::endl;
+            
+        // read_children:
+        //     int number_of_children;
+        //     ifstream >> number_of_children;
+        //     std::cout << number_of_children << std::endl;
+        //     for (int i = 0; i < number_of_children; i++) {
+        //         Node* child = new Node("", nullptr, 0);
+        //         child->read_file_recursively(ifstream);
+        //         children.push_back(child);
+        //     }
+        // }
 };
